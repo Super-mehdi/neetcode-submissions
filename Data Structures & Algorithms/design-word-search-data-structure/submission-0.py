580b3d71afd115b24706876838f211d25117d3fe
El mehdi Amghary <Amgharyelmehdi@gmail.com>
2026-08-08 17:18:37 +0100

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.py
@@ -0,0 +1,42 @@
+class DictionaryNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
+
+class WordDictionary:
+    def __init__(self):
+        self.root = DictionaryNode()
+
+    def addWord(self, word: str) -> None:
+        currNode = self.root
+        for c in word:
+            if c not in currNode.children :
+                currNode.children[c] = DictionaryNode()
+            currNode = currNode.children[c]
+        currNode.endOfWord = True
+        
+
+    def search(self, word: str) -> bool:
+        def dfs(j, root):
+            curr = root
+            for i in range(j, len(word)):
+                c = word[i]
+                if c == '.':
+                    for child in curr.children.values():
+                        if dfs(i+1,child):
+                            return True
+                    return False
+                else:
+                    if c not in curr.children:
+                        return False
+                    curr = curr.children[c]
+            return curr.endOfWord
+        return dfs(0,self.root)
+                
+
+
+
+
+
+
+        
